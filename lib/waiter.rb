b375a9b13b560650f3e58fa1ffaf3dc135e32ff3
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 18:22:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
 class Waiter
 
+  attr_accessor :name, :yrs_expirience
+
+  @@all = []
+
+  def initialize
+    @name = name
+    @yrs_expirience = yrs_expirience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
+  end
 end
